9441f297289ba935b68e2e20a043e27d544c0f96
Cayde Dixon <cayde.dixon@gmail.com>
2014-07-01 13:49:05 +1000

    Fixed up devise so username XOR email login works.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login, :email, :username]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   attr_accessor :login
 
+  validates :username, :uniqueness => { :case_sensitive => false }
+
   has_many :posts, dependent: :destroy
-
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.username || self.email
-  end
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
