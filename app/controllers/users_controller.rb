b1a3a5980a67802d27a229a1aecbf462471328b6
Cayde Dixon <cayde.dixon@gmail.com>
2014-07-02 20:30:33 +1000

    Inital work on the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+	before_filter :check_admin!
+	before_action :authenticate_user!
+
+	def index
+		@users = User.paginate(:page => params[:page], :per_page => 20).order('created_at DESC')
+	end
 end
